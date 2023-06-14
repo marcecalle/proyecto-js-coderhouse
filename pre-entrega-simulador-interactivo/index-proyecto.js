16e6588aa3689b02d3a19dc0ef2d9ac6d4186ac9
marcecalle <38355063+marcecalle@users.noreply.github.com>
2023-06-14 18:21:45 -0300

    mejora en la maquetacion de html y css, mejora en el simulador (1.1)

--- pre-entrega-simulador-interactivo/index-proyecto.js
+++ pre-entrega-simulador-interactivo/index-proyecto.js
@@ -1,32 +1,41 @@
-// SIMULADOR INTERACTIVO DE UNA SUBASTA DE ILUSTRACIÓNES
+//* SIMULADOR INTERACTIVO DE UNA SUBASTA DE ILUSTRACIÓNES
 
 // Declaración y asignación de variables
-let valorItem = 0
-let consentimientoSubasta = prompt('Bienvenido ¿Quiere realizar una oferta? si / no')
+
+let valorItem = 0;
+let consentimientoSubasta = prompt(
+  "Bienvenido ¿Quiere realizar una oferta? si / no"
+);
 
 // Función de validación por SI o por NO para iniciar la subasta
+
 function ingresoSubasta(consentimiento) {
-    if (consentimiento === 'si') {
-        return consentimientoSubasta
-    } else { 
-    alert('Gracias, vuelva pronto.');
-    }
+  if (consentimiento === "si") {
+    return consentimientoSubasta;
+  }
+  if (consentimiento === "no") {
+    alert("Gracias, vuelva pronto.");
+  }
+  if (consentimiento !== "si" && consentimiento !== "no") {
+    alert("La opción ingresada no es valida.");
+  }
 }
 
 // función para que el usuario realice una oferta
+
 function ofertaUsuario(valorReferencia) {
-    alert('La oferta actual es $' + valorReferencia)
-    valorOferta = Number(prompt('Ingrese su oferta por favor.'))
-    if (valorOferta > valorReferencia) {
-        valorItem = valorOferta
-        alert('Su oferta de $' + valorOferta + ' fue aceptada, muchas gracias')
-    } else {
-        alert('Su oferta de $' + valorOferta + ' no es superadora.')
-    }
+  alert("La oferta actual es $" + valorReferencia);
+  valorOferta = Number(prompt("Ingrese su oferta por favor."));
+  if (valorOferta > valorReferencia) {
+    valorItem = valorOferta;
+    alert("Su oferta de $" + valorOferta + " fue aceptada, muchas gracias");
+  } else {
+    alert("Su oferta de $" + valorOferta + " no es superadora.");
+  }
 }
 
 // Ciclo simulador de la validación y la oferta de la subasta
-while (ingresoSubasta(consentimientoSubasta) === 'si') {
-    ofertaUsuario(valorItem)
-    consentimientoSubasta = prompt('¿Quiere realizar otra oferta? si / no')
+while (ingresoSubasta(consentimientoSubasta) === "si") {
+  ofertaUsuario(valorItem);
+  consentimientoSubasta = prompt("¿Quiere realizar otra oferta? si / no");
 }
